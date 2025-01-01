b59eda2d6cd877520ceba74844775cec09c5c3dd
jonathan-robertson <jrob356@icloud.com>
2025-01-01 15:43:50 -0600

    finalize fix, for now

--- README.md
+++ README.md
@@ -15,20 +15,91 @@
 
 ## Features
 
-TODO
+This client-side mod addresses an exploit that currently allows players to detect underground tunnels by holding out any multi-dim (multi-dimension) block which also doesn't support stability.
+
+> 🛈 This can be used to identify any gap in the ground between bedrock and your placement location at any distance.
+
+An example of this kind of block would be the `Workbench` or a `bale of hay`.
+
+### Who is this mod for?
+
+- This is primarily meant for demonstration purposes to help with a bug report I plan to file with The Fun Pimps, designers of 7 Days to Die.
+- *Maybe* Overhaul modders would also benefit from this, but probably not anyone else (since this is client-side only, due to limitations within the game's systems).
 
 ### Admin Commands
 
-> ℹ️ You can always search for this command or any command by running:
+> 🛈 You can always search for this command or any command by running:
 >
 > - `help * <partial or complete command name>`
 > - or get details about this (or any) command and its options by running `help <command>`
 
-| primary | alternate |     params     | description                                                     |
-| :-----: | :-------: | :------------: | --------------------------------------------------------------- |
-|  TODO   |   TODO    | `debug` / `dm` | enable/disable debug logging for this mod (disabled by default) |
+|        primary         | alternate |     params     | description                                                           |
+| :--------------------: | :-------: | :------------: | --------------------------------------------------------------------- |
+| `multidimstabilityfix` |  `mdsf`   | `debug` / `dm` | enable/disable debug/trace logging for this mod (disabled by default) |
 
 *Note that leaving debug mode on can have a negative impact on performance. It is therefore recommended to only turn it on while troubleshooting and then disable it afterwards.*
+
+## The Exploit Explained
+
+Some odd behavior exists that can be used to take advantage of locating underground player bases for raiding purposes.
+
+Context: a block that...
+
+- has `MultiBlockDim` set to a Y/height value greater than 1
+- also has `StabilitySupport` set to `false`
+
+The adjustment I made was in `StabilityCalculator.GetBlockStabilityIfPlaced`
+
+1. Near the beginning of this method is a check for `if (block.isMultiBlock)`... which it is in this case.
+2. Each of the blocks that make up this Multi-Dimension block are collected while the minimum and maximum vector positions are determined.
+3. After this, some additional work is performed which I won't get into...
+4. And finally, each block is analyzed for stability mechanics.
+   - *this is where the problem takes place*
+
+For reasons I don't perfectly understand and will not speculate on, the game starts by checking the block positions at the top of the multi-dim block and this results in a stability failure - if and only if there is not a complete chain of blocks (standard blocks or terrain) between the proposed placement location and bedrock.
+
+> 🛈 The client-side callstack goes PlayerMoveController.Update -> RenderDisplacedCube.Update -> RenderDisplacedCube.update0 -> StabilityCalculator.GetBlockStabilityIfPlaced.
+>
+> ⚠️ This is a client-side issue that cannot be addressed by a server-side mod; HELP US, FUN PIMPS - YOU'RE OUR ONLY HOPE!!
+
+### Possible Fix Proposal
+
+So how can we fix this?
+
+My approach is to skip any blocks which are not identified as the lowest layer of blocks within this Multi-Dimension block.
+
+Once adjusted, multi-dim blocks of height > 1 will suggest stability just like other multi-dim blocks of height 1 or non-multi-dim blocks do.
+
+Inside, we find this block of code (keep in mind, this is decompiled, so the variable names do not reflect source):
+
+```cs
+// ...
+for (var i = block.multiBlockPos.Length - 1; i >= 0; i--)
+{
+    var vector3i3 = block.multiBlockPos.Get(i, type, rotation) + _pos;
+    if (!StabilityCalculator.posPlaced.ContainsKey(vector3i3))
+    {
+        StabilityCalculator.posPlaced.Add(vector3i3, num);
+        vector3i = Vector3i.Min(vector3i, vector3i3); // <----- NOTICE THIS; WE REFER TO IT BELOW
+        vector3i2 = Vector3i.Max(vector3i2, vector3i3);
+    }
+}
+// ...
+using (var enumerator = StabilityCalculator.posPlaced.Keys.GetEnumerator())
+{
+    while (enumerator.MoveNext())
+    {
+        var vector3i5 = enumerator.Current;
+        // === START NEW =========================================================================
+        if (vector3i5.y != vector3i.y) // verify if this block position is at base of multi-dim
+        {
+            continue; // skip block positions which are not along the lower edge of this multi-dim
+        }
+        // === END NEW ===========================================================================
+        if (vector3i5.x == vector3i.x || vector3i5.x == vector3i2.x || vector3i5.y == vector3i.y || vector3i5.y == vector3i2.y || vector3i5.z == vector3i.z || vector3i5.z == vector3i2.z)
+        {
+          // ...
+```
 
 ## Setup
 
@@ -40,11 +111,11 @@ If you have trouble getting things working, you can reach out to me for support 
 
 | Environment          | Compatible | Does EAC Need to be Disabled? | Who needs to install? |
 | -------------------- | ---------- | ----------------------------- | --------------------- |
-| Dedicated Server     | No         | N/A                           | N/A                   |
-| Peer-to-Peer Hosting | No         | N/A                           | N/A                   |
 | Single Player Game   | Yes        | Yes                           | self (of course)      |
+| Peer-to-Peer Hosting | No         | N/A                           | N/A                   |
+| Dedicated Server     | No         | N/A                           | N/A                   |
 
-> 🤔 If you aren't sure what some of this means, details steps are provided below to walk you through the setup process.
+> 🛈 If you aren't sure what some of this means, details steps are provided below to walk you through the setup process.
 
 ### Map Considerations for Installation or UnInstallation
 
@@ -55,7 +126,7 @@ If you have trouble getting things working, you can reach out to me for support 
 
 ### Windows PC (Single Player)
 
-> ℹ️ If you plan to host a multiplayer game, only the host PC will need to install this mod. Other players connecting to your session do not need to install anything for this mod to work 😉
+> 🛈 Please remember that this mod is not going to work as expected in multiplayer. It is a client-side concept mod only.
 
 1. 📦 Download the latest release by navigating to [this link](https://github.com/jonathan-robertson/multi-dim-stability-fix/releases/latest/) and clicking the link for `multi-dim-stability-fix.zip`
 2. 📂 Unzip this file to a folder named `multi-dim-stability-fix` by right-clicking it and choosing the `Extract All...` option (you will find Windows suggests extracting to a new folder named `multi-dim-stability-fix` - this is the option you want to use)
@@ -66,8 +137,4 @@ If you have trouble getting things working, you can reach out to me for support 
 
 #### Critical Reminders
 
-- ⚠️ it is **NECESSARY** for the host to run with EAC disabled or the DLL file in this mod will not be able to run
-- 😉 other players **DO NOT** need to disable EAC in order to connect to your game session, so you don't need to walk them through these steps
-- 🔑 it is also **HIGHLY RECOMMENDED** to add a password to your game session
-  - while disabling EAC is 100% necessary (for P2P or single player) to run this mod properly, it also allows other players to run any mods they want on their end (which could be used to gain access to admin commands and/or grief you or your other players)
-  - please note that *dedicated servers* do not have this limitation and can have EAC fully enabled; we have setup guides for dedicated servers as well, listed in the next 2 sections: [Windows/Linux Installation (Server via FTP from Windows PC)](#windowslinux-installation-server-via-ftp-from-windows-pc) and [Linux Server Installation (Server via SSH)](#linux-server-installation-server-via-ssh)
+- ⚠️ it is **NECESSARY** for you to to run with EAC disabled or the DLL file in this mod will not be able to run
--- src/Patches/StabilityCalculator.cs
+++ src/Patches/StabilityCalculator.cs
@@ -1,5 +1,6 @@
 ﻿using HarmonyLib;
 using MultiDimStabilityFix.Utilities;
+using System;
 
 namespace MultiDimStabilityFix.Patches
 {
@@ -10,79 +11,102 @@ namespace MultiDimStabilityFix.Patches
 
         public static bool Prefix(Vector3i _pos, BlockValue _bv, ref float __result)
         {
-            _log.Info("Prefix");
-
-            var microStopwatch = new MicroStopwatch();
-            var block = _bv.Block;
-            var num = block.StabilitySupport ? 1 : 0;
-            float num2;
-            if (block.isMultiBlock)
+            try
             {
-                var type = _bv.type;
-                var rotation = (int)_bv.rotation;
-                var vector3i = Vector3i.max;
-                var vector3i2 = Vector3i.min;
-                for (var i = block.multiBlockPos.Length - 1; i >= 0; i--)
+                _log.Trace("Prefix start");
+
+                var microStopwatch = new MicroStopwatch();
+                var block = _bv.Block;
+                var num = block.StabilitySupport ? 1 : 0;
+                float num2;
+                if (block.isMultiBlock)
                 {
-                    var vector3i3 = block.multiBlockPos.Get(i, type, rotation) + _pos;
-                    if (!StabilityCalculator.posPlaced.ContainsKey(vector3i3))
+                    var type = _bv.type;
+                    var rotation = (int)_bv.rotation;
+                    var vector3i = Vector3i.max;
+                    var vector3i2 = Vector3i.min;
+                    for (var i = block.multiBlockPos.Length - 1; i >= 0; i--)
                     {
-                        StabilityCalculator.posPlaced.Add(vector3i3, num);
-                        vector3i = Vector3i.Min(vector3i, vector3i3);
-                        vector3i2 = Vector3i.Max(vector3i2, vector3i3);
-                    }
-                }
-                IChunk chunk = null;
-                for (var j = vector3i.z; j <= vector3i2.z; j++)
-                {
-                    Vector3i vector3i4;
-                    vector3i4.z = j;
-                    for (var k = vector3i.x; k <= vector3i2.x; k++)
-                    {
-                        vector3i4.x = k;
-                        StabilityCalculator.world.GetChunkFromWorldPos(k, j, ref chunk);
-                        if (chunk != null && chunk.GetStability(k & 15, vector3i.y - 1, j & 15) == 15)
+                        var vector3i3 = block.multiBlockPos.Get(i, type, rotation) + _pos;
+                        if (!StabilityCalculator.posPlaced.ContainsKey(vector3i3))
                         {
-                            for (var l = vector3i.y; l <= vector3i2.y; l++)
+                            _log.Trace($"adding position {vector3i3} to stability calculator with value of {num}");
+                            StabilityCalculator.posPlaced.Add(vector3i3, num);
+                            vector3i = Vector3i.Min(vector3i, vector3i3);
+                            vector3i2 = Vector3i.Max(vector3i2, vector3i3);
+                        }
+                        _log.Trace($"dimensions updated to min: {vector3i}, max: {vector3i2}");
+                    }
+                    IChunk chunk = null;
+                    _log.Trace($"dimensions verified @ min: {vector3i}, max: {vector3i2}");
+                    for (var j = vector3i.z; j <= vector3i2.z; j++)
+                    {
+                        Vector3i vector3i4;
+                        vector3i4.z = j;
+                        for (var k = vector3i.x; k <= vector3i2.x; k++)
+                        {
+                            vector3i4.x = k;
+                            StabilityCalculator.world.GetChunkFromWorldPos(k, j, ref chunk);
+                            _log.Trace($"chunk != null? {chunk != null} && chunk.GetStability(k:{k} & 15: {k & 15}, vector3i.y:{vector3i.y}-1, j:{j} & 15: {j & 15}): {chunk.GetStability(k & 15, vector3i.y - 1, j & 15)} == 15):{chunk.GetStability(k & 15, vector3i.y - 1, j & 15) == 15}");
+                            if (chunk != null && chunk.GetStability(k & 15, vector3i.y - 1, j & 15) == 15)
                             {
-                                vector3i4.y = l;
-                                StabilityCalculator.posPlaced[vector3i4] = 15;
+                                for (var l = vector3i.y; l <= vector3i2.y; l++)
+                                {
+                                    vector3i4.y = l;
+                                    StabilityCalculator.posPlaced[vector3i4] = 15;
+                                    _log.Trace($"vector3i4.y: {vector3i4.y}");
+                                }
                             }
                         }
                     }
-                }
-                num2 = 1f;
-                using (var enumerator = StabilityCalculator.posPlaced.Keys.GetEnumerator())
-                {
-                    while (enumerator.MoveNext())
+                    num2 = 1f;
+                    using (var enumerator = StabilityCalculator.posPlaced.Keys.GetEnumerator())
                     {
-                        var vector3i5 = enumerator.Current;
-                        if (vector3i5.x == vector3i.x || vector3i5.x == vector3i2.x || vector3i5.y == vector3i.y || vector3i5.y == vector3i2.y || vector3i5.z == vector3i.z || vector3i5.z == vector3i2.z)
+                        while (enumerator.MoveNext())
                         {
-                            var blockStability = StabilityCalculator.GetBlockStability(vector3i5, _bv);
-                            if (blockStability < num2)
+                            var vector3i5 = enumerator.Current;
+                            // === NEW ===============================================================================
+                            if (vector3i5.y != vector3i.y)
                             {
-                                num2 = blockStability;
-                                if (blockStability == 0f)
+                                _log.Trace($"skipping: {vector3i5}");
+                                continue; // skip block positions which are not along the lower edge of this multi-dim
+                            }
+                            // === END NEW ===========================================================================
+                            _log.Trace($"checking: {vector3i5}");
+                            if (vector3i5.x == vector3i.x || vector3i5.x == vector3i2.x || vector3i5.y == vector3i.y || vector3i5.y == vector3i2.y || vector3i5.z == vector3i.z || vector3i5.z == vector3i2.z)
+                            {
+                                _log.Trace($"StabilityCalculator.GetBlockStability(vector3i5: {vector3i5}, _bv: {_bv})");
+                                var blockStability = StabilityCalculator.GetBlockStability(vector3i5, _bv);
+                                _log.Trace($"blockStability: {blockStability} vs num2: {num2}");
+                                if (blockStability < num2)
+                                {
+                                    num2 = blockStability;
+                                    if (blockStability == 0f)
+                                    {
+                                        break;
+                                    }
+                                }
+                                if (microStopwatch.ElapsedMicroseconds > 25000L)
                                 {
                                     break;
                                 }
                             }
-                            if (microStopwatch.ElapsedMicroseconds > 25000L)
-                            {
-                                break;
-                            }
                         }
+                        goto IL_246;
                     }
-                    goto IL_246;
                 }
-            }
-            StabilityCalculator.posPlaced.Add(_pos, num);
-            num2 = StabilityCalculator.GetBlockStability(_pos, _bv);
-        IL_246:
-            StabilityCalculator.posPlaced.Clear();
+                StabilityCalculator.posPlaced.Add(_pos, num);
+                num2 = StabilityCalculator.GetBlockStability(_pos, _bv);
+            IL_246:
+                StabilityCalculator.posPlaced.Clear();
 
-            __result = num2;
+                _log.Trace($"setting result to {num2}");
+                __result = num2;
+            }
+            catch (Exception e)
+            {
+                _log.Error("Prefix", e);
+            }
             return false;
         }
     }
